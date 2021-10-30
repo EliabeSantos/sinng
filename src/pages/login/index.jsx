1e35aa8d15c899d2247811a751e3a15de72498a8
Eliabe Santos <eliabedodo@gmail.com>
2021-10-30 06:11:26 -0300

    Login page comecada

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,12 @@
+import {MainContainer} from "./style"
+
 const Login = () => {
 
-    return <>
-        <div>Teste loko</div>
-    </>
+    return <MainContainer>
+        <h1>Logo</h1>
+        <input type="email" placeholder="Digite seu e-mail" />
+        <input type="password" placeholder="Digite seu e-mail" />
+    </MainContainer>
 }
 
 export default Login
